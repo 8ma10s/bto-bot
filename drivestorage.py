51c1735d040ad5fe16e0d0f92475d7f82d4f8401
Yamato Sasaki <yamatosasaki14@gmail.com>
2018-05-29 19:56:00 -0700

    Add files.ini

--- drivestorage.py
+++ drivestorage.py
@@ -47,59 +47,6 @@ class DriveStorage:
         return data
 
 
-
-            
-        """ for dir in targetPath:
-            if dir in curDir['entries']:
-                if not curDir['entries'][dir]['isDir']:
-                    raise error.NotADirectoryError('name: "' + dir + '" is not a directory')
-                curDir = curDir['entries'][dir]
-            else:
-                self.__refreshDir(curDir['id'], curDir)
-                if dir in curDir['entries']:
-                    if not curDir['entries'][dir]['isDir']:
-                        raise error.NotADirectoryError('name: "' + dir + '" is not a directory')
-                    curDir = curDir['entries'][dir]
-                else:
-                    # create new folder
-                    newFolder = self.drive.CreateFile(
-                        {'title':dir,
-                        'parents':[{'id': curDir['id']}],
-                        "mimeType": "application/vnd.google-apps.folder"
-                        })
-                    newFolder.Upload()
-                    curDir['entries'][dir] = {'isDir': True, 'id': newFolder['id'], 'entries': {}}
-                    curDir = curDir['entries'][dir]
-        
-        
-        self.__refreshDir(curDir['id'], curDir)
-        # update file
-        if filename in curDir['entries']:
-            gFile = self.drive.CreateFile(
-                {'title':filename, 
-                'id': curDir['entries'][filename]['id'],
-                'parents':[{'kind': 'drive#fileLink', 'id': curDir['id']}]
-                })
-            
-        else:
-            gFile = self.drive.CreateFile(
-                {'title':filename,
-                'parents':[{'kind': 'drive#fileLink', 'id': curDir['id']}]
-                })
-
-
-        gFile.content = data
-        gFile.Upload()
-        self.__reduceCache()
-        if gFile['id'] in self.dataCache:
-            self.cacheSize -= self.dataCache[gFile['id']].getbuffer().nbytes
-            self.dataCache[gFile['id']].close()
-        self.dataCache[gFile['id']] = data
-        self.cacheSize += data.getbuffer().nbytes
-        self.__listFiles.cache_clear()
-        curDir['entries'][filename] = {'id': gFile['id'], 'isDir':False, 'entries':{}}
-        return data"""
-
     def download(self, location, filenames=[], exact=False):
         if exact and len(filenames) != 1:
             raise error.InvalidParametersError('if exact=True, there must only be 1 filename argument')
@@ -139,103 +86,11 @@ class DriveStorage:
         self.dataCache[id].seek(0)
         return (name, self.dataCache[id])
 
-        """targetPath = location
-        curDir = self.dirIndex['root']
-        for dir in targetPath:
-            if dir in curDir['entries']:
-                if not curDir['entries'][dir]['isDir']:
-                    raise error.NotADirectoryError('name: "' + dir + '" is not a directory')
-                curDir = curDir['entries'][dir]
-            else:
-
-                self.__refreshDir(curDir['id'], curDir)
-                if dir in curDir['entries']:
-                    if not curDir['entries'][dir]['isDir']:
-                        raise error.NotADirectoryError('name: "' + dir + '" is not a directory')
-                    curDir = curDir['entries'][dir]
-                else:
-                    raise error.DirectoryNotFoundError('The specified directory: "' +  dir + '" was not found')
-
-        # if cached, no need to connect to Google
-        if filenames: 
-            for key in list(curDir['entries'].keys()):
-                matchObj = curDir['entries'][key]
-                if all(filename in key for filename in filenames) and not matchObj['isDir'] and matchObj['id'] in self.dataCache:
-                    self.dataCache.move_to_end(matchObj['id'])
-                    print('cached, no list load')
-                    return (key, self.dataCache[matchObj['id']])
-
-        gFiles = self.drive.ListFile({'q': "'" + curDir['id'] + "' in parents and trashed=false"}).GetList()
-        gFiles = list(filter(lambda x: all(y in x['title'] for y in filenames), gFiles))
-
-        if not gFiles:
-            raise error.FileNotFoundError('Could not find a file that matches all queries')
-        # choose exact match if exists. If not, choose a random file among the ones that matched the query
-        gFile = None
-        if len(filenames) == 1:
-            for match in gFiles:
-                print(match['title'])
-                if os.path.splitext(match['title'])[0] == os.path.splitext(filenames[0])[0]:
-                    gFile = match
-        if not gFile:
-            gFile = random.choice(gFiles)
-        
-        # if cached, return cache
-        if gFile['id'] in self.dataCache:
-            self.dataCache.move_to_end(gFile['id'])
-            print('cached, list load')
-            return (gFile['title'], self.dataCache[gFile['id']])
-        else:
-            gFile.FetchContent()
-            self.__reduceCache()
-            self.dataCache[gFile['id']] = gFile.content
-            self.cacheSize += gFile.content.getbuffer().nbytes
-
-            curDir['entries'][gFile['title']] = {'id': gFile['id'], 'isDir':False, 'entries':{}}
-            print('not cached')
-            return (gFile['title'], gFile.content) """
-    
 
     def listFiles(self, location, filenames=[]):
         curDir = self.__getDirectory(location, self.dirIndex['root'])
         return [name for name in curDir['entries'] if all(filename in name for filename in filenames)]
 
-    """
-    @lru_cache()
-    def __listFiles(self, location):
-        targetPath = list(location)
-        curDir = self.dirIndex['root']
-        for dir in targetPath:
-            if dir in curDir['entries']:
-                if not curDir['entries'][dir]['isDir']:
-                    raise error.NotADirectoryError('name: "' + dir + '" is not a directory')
-                curDir = curDir['entries'][dir]
-            else:
-
-                self.__refreshDir(curDir['id'], curDir)
-                if dir in curDir['entries']:
-                    if not curDir['entries'][dir]['isDir']:
-                        raise error.NotADirectoryError('name: "' + dir + '" is not a directory')
-                    curDir = curDir['entries'][dir]
-                else:
-                    raise error.DirectoryNotFoundError('The specified directory: "' +  dir + '" was not found')
-        
-        print("'" + curDir['id'] + "' in parents and trashed=false")
-        gFiles = self.drive.ListFile({'q': "'" + curDir['id'] + "' in parents and trashed=false"}).GetList()
-        return [gFile['title'] for gFile in gFiles]
-        
-
-
-
-
-    
-    def __refreshDir(self, id, curDir):
-        ## make a dictionary of subdirectories, with key being the title, and values being dict of id, isDir, and empty subDir
-        ## then set it equal to current folder's subdirectory
-        curDir['entries'] = { result['title']:{'id':result['id'], 
-        'isDir':(True if result['mimeType'] == 'application/vnd.google-apps.folder' else False), 
-        'entries': {} if result['title'] not in curDir['entries'] else curDir['entries'][result['title']]['entries']} 
-        for result in self.drive.ListFile({'q': "'" + id + "' in parents and trashed=false"}).GetList() } """
 
     def __reduceCache(self):
         if self.cacheSize > 262144000:
